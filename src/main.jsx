4d09699dee1088b214f438a0b02d86976c5816aa
Romance Ucely <faaa@live.com>
2023-04-21 00:35:59 +0800

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Canvas } from '@react-three/fiber'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Canvas>
+    <mesh>
+      <torusKnotBufferGeometry />
+      <meshNormalMaterial />
+    </mesh>
+  </Canvas>
+)
